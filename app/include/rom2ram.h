6e111c7da391a90c40deb20ef3f66b966bc4ba74
pvvx <pvvx@mail.ru>
2015-07-01 05:02:25 +0300

    add

---
+++ app/include/rom2ram.h
@@ -0,0 +1,19 @@
+/*
+ * rom2ram.h
+ * Author: pvvx
+ */
+
+#ifndef _INCLUDE_ROM2RAM_H_
+#define _INCLUDE_ROM2RAM_H_
+
+#define USE_ROM_STRINGS
+
+extern void copy_s4d1(unsigned char * pd, void * ps, unsigned int len);
+extern void copy_s1d4(void * pd, unsigned char * ps, unsigned int len);
+extern unsigned int rom_strlen(const char *s);
+extern const char * rom_strchr(const char *s, char c);
+extern const char get_rom_chr(const char *s);
+extern const char * rom_strcpy(char * pd_, void * ps, unsigned int maxlen);
+
+
+#endif /* _INCLUDE_ROM2RAM_H_ */
